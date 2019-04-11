ff4d82b43a74f9817909f23e1fdc0470156a38ea
= <=>
2019-04-11 14:37:15 -0700

    set up server, massive, controllers, register endpoint

---
+++ server/index.js
@@ -0,0 +1,27 @@
+require('dotenv').config()
+const express = require('express');
+const app = express()
+const massive = require('massive')
+const session = require('express-session')
+const { PORT, CONNECTION_STRING, SESSION_SECRET } = process.env
+const authController = require('./controllers/authController')
+
+app.use(express.json())
+app.use(session({
+    resave: true,
+    saveUninitialized: false,
+    secret: SESSION_SECRET
+}))
+
+massive(CONNECTION_STRING)
+    .then(db => { 
+        app.set('db', db)
+        console.log('db connected')
+    })
+
+    app.post('/auth/register', authController.register)
+
+app.listen(PORT || 4000, () => { 
+    console.log(`Server has strted on port ${PORT}`)
+})
+
