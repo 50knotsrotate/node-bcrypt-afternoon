6a26b594a345bbcaf4c311d4a74d99a427450505
= <=>
2019-04-11 14:53:37 -0700

    installed bcrypt, backend auth for registering new user

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,5 +1,25 @@
+const bcrypt = require('bcryptjs')
 module.exports = {
-    register: (req, res) => { 
-        res.status(200).send('ayyyyyy')
+    register: async (req, res) => { 
+        const { username, password, isAdmin } = req.body
+        const db = req.app.get('db')
+        const existingUser = await db.get_user()
+        if (existingUser) {
+            res.status(409).send('Username is taken')
+        } else { 
+            const salt = bcrypt.genSalt(12)
+            const hash = bcrypt.hash(password, salt)
+            
+            const registeredUser = await db.register_user([isAdmin, username, hash])
+            const user = registeredUser[0]
+
+            req.session.user = {
+                isAdmin: user.isAdmin,
+                id: user.id, 
+                username: user.username
+            }
+            return res.status(201).send(req.session.user)
+          
+        }
     }
 }
